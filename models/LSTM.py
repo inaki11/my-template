c8882bf795c7d507f5ebff4ff58bf25f3368601e
inaki11 <inakitodc@gmail.com>
2025-07-18 02:39:42 +0200

    añado imagen de la primera semana de test, y modelo LSTM de prueba

---
+++ models/LSTM.py
@@ -0,0 +1,41 @@
+import typing as ty
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, output_size, config):
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=config.hidden_size,
+            num_layers=config.num_layers,
+            batch_first=True,
+            dropout=config.dropout,
+        )
+        self.fc = nn.Linear(config.hidden_size, output_size)
+
+    def forward(self, x):
+
+        return x
+
+
+def build_model(input_size, output_size, config):
+    # Extract model parameters from the config (assumes they are under the "model" key)
+    # first assert that all required keys are present
+    print("Building LSTM model with config:", config)
+    required_keys = [
+        "hidden_size",
+        "num_layers",
+        "dropout",
+    ]
+    for key in required_keys:
+        if key not in config:
+            raise ValueError(f"Missing required model configuration key: {key}")
+
+    return LSMT(
+        input_size=input_size,
+        output_size=output_size,
+        config=config,
+    )
--- utils/plot_inverse_transformed_outputs_and_mae.py
+++ utils/plot_inverse_transformed_outputs_and_mae.py
@@ -32,6 +32,24 @@ def plot_inverse_transformed_outputs_and_mae(outputs, targets, fold):
             )
         }
     )
+    # Cierra la figura para liberar memoria
+    plt.close()
 
+    plt.figure(figsize=(10, 5))
+    plt.plot(targets[:144], label="Targets", color="green")
+    plt.plot(outputs[:144], label="Outputs", color="orange")
+    plt.title(f"Fold {fold} - Outputs vs Targets 1st week\nMAE: {mae:.4f}")
+    plt.ylabel("traffic flow")
+    plt.legend()
+    plt.grid()
+
+    # Loggear la imagen a wandb
+    wandb.log(
+        {
+            f"Outputs_vs_Targets_1st_week_Fold_{fold}": wandb.Image(
+                plt.gcf(), caption=f"MAE: {mae:.4f}"
+            )
+        }
+    )
     # Cierra la figura para liberar memoria
     plt.close()
